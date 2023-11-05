b1ea1d0d8a0c1b8331af5e1bb2497f3d7d6a0ae6
Robin Holzinger <robin.holzinger@tum.de>
2023-11-05 02:01:15 +0100

    Add tremor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Card, Text, Metric, Flex, ProgressBar } from '@tremor/react'
 
 export default function Home() {
   return (
@@ -20,15 +21,17 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <Card className="mx-auto max-w-xs">
+          <Text>Sales</Text>
+          <Metric>$ 71,465</Metric>
+          <Flex className="mt-4">
+            <Text>32% of annual target</Text>
+            <Text>$ 225,000</Text>
+          </Flex>
+          <ProgressBar value={32} className="mt-2" />
+        </Card>
+        {/* TODO: continue with https://www.tremor.so/docs/getting-started/demo-dashboard */}
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">

c5f9860da6ca80c64dcc55a883f82c99ffa54686
Deepankar Ratra <deepankar@LAPTOP-P0KHNUO5.localdomain>
2023-11-23 07:44:20 +0100

    feat:Adding APIs based on data model entities

---
+++ pages/api/notification/index.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import notificationService from '@/api/services/notificationService'
+const { createNotification } = notificationService
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req
+
+  switch (method) {
+    case 'POST':
+      try {
+        const notification = await createNotification(req.body)
+        if (notification) {
+          res.status(201).json(notification)
+        } else res.status(400).json({ error: 'Invalid request parameters' })
+      } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' })
+      }
+      break
+
+    default:
+      res.status(405).json({ error: 'Method Not Allowed' })
+      break
+  }
+}

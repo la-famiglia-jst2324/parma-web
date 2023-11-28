53a32b8eb93e16ba5d5993068adf97efe99c191b
Robin Holzinger <robin.holzinger@tum.de>
2023-11-28 21:05:34 +0100

    Provide admin certificate through env in CI/prod/

--- src/pages/api/lib/firebase/main.ts
+++ src/pages/api/lib/firebase/main.ts
@@ -1,9 +1,21 @@
+import { existsSync, readFileSync } from 'fs'
 import * as admin from 'firebase-admin'
-import * as serviceAccount from '../../.secrets/la-famiglia-parma-ai-firebase-adminsdk.json'
 
 if (admin.apps.length === 0) {
+  const certFilePath = '../../.secrets/la-famiglia-parma-ai-firebase-adminsdk.json'
+  let certString = null
+  if (existsSync(certFilePath)) {
+    certString = readFileSync(certFilePath, 'utf8')
+  } else {
+    certString = process.env.FIREBASE_ADMINSDK_CERTIFICATE
+  }
+
+  if (!certString) {
+    throw new Error('FIREBASE_ADMINSDK_CERTIFICATE not found')
+  }
+
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    credential: admin.credential.cert(JSON.parse(certString))
   })
 }
 

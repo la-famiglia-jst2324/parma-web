0a1eae1e768b38bda74e1533adb42c916a2ee151
Hamza <chaoukihamza16@gmail.com>
2023-11-30 14:12:49 +0100

    Adding tests for dashboard components

---
+++ tests/dashboard/news_card_component.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import NewsCard from '@/components/Dashboard/NewsCard';
+
+describe('NewsCard', () => {
+  const mockNewsItem = {
+    id:1,
+    title: 'Test Title',
+    companyName: 'Test Company',
+    datasourceName: 'Test Datasource',
+    timestamp: '2022-01-01',
+    description: 'Test Description',
+    link: 'https://test.com',
+    icon: '/path/to/icon.png',
+  };
+
+  it('renders without crashing', () => {
+    render(<NewsCard {...mockNewsItem} />);
+    expect(screen.getByText('Test Title')).toBeInTheDocument();
+  });
+
+  it('renders the company name', () => {
+    render(<NewsCard {...mockNewsItem} />);
+    expect(screen.getByText('Test Company')).toBeInTheDocument();
+  });
+
+  it('renders the datasource name', () => {
+    render(<NewsCard {...mockNewsItem} />);
+    expect(screen.getByText('Test Datasource')).toBeInTheDocument();
+  });
+
+  it('renders the timestamp', () => {
+    render(<NewsCard {...mockNewsItem} />);
+    expect(screen.getByText('2022-01-01')).toBeInTheDocument();
+  });
+
+  it('renders the description', () => {
+    render(<NewsCard {...mockNewsItem} />);
+    expect(screen.getByText('Test Description')).toBeInTheDocument();
+  });
+
+  it('renders the link', () => {
+    render(<NewsCard {...mockNewsItem} />);
+    expect(screen.getByText('Read full story here')).toHaveAttribute('href', 'https://test.com');
+  });
+});
---
+++ tests/dashboard/top_buckets_component.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import TopBucketsCard from '@/components/Dashboard/TopBucketsCard';
+
+describe('TopBucketsCard Test', () => {
+  const mockBucketItem = {
+    id: 1,
+    name: 'Test Bucket',
+    description: 'Test Description',
+    numberOfCompanies: 5,
+  };
+
+  it('renders without crashing', () => {
+    render(<TopBucketsCard {...mockBucketItem} />);
+    expect(screen.getByText('Test Bucket')).toBeInTheDocument();
+  });
+
+  it('renders the number of companies', () => {
+    render(<TopBucketsCard {...mockBucketItem} />);
+    expect(screen.getByText('5 companies')).toBeInTheDocument();
+  });
+
+  it('renders the description', () => {
+    render(<TopBucketsCard {...mockBucketItem} />);
+    expect(screen.getByText('Test Description')).toBeInTheDocument();
+  });
+
+  it('renders the view more link', () => {
+    render(<TopBucketsCard {...mockBucketItem} />);
+    expect(screen.getByText('View More')).toHaveAttribute('href', '/buckets/1');
+  });
+});

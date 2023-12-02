5fd9457af75d3b981066a6f437aba5526a95c40b
Hamza <70699922+HamzaChx@users.noreply.github.com>
2023-12-02 18:17:07 +0100

    feat: Integrate API into Datasources (#52)
    Co-authored-by: Robin Holzinger <robin.holzinger@tum.de>

--- src/components/FormContent.tsx
+++ src/components/FormContent.tsx
@@ -5,21 +5,37 @@ interface FormContentProps {
   label: string
   name: string
   placeholder: string
+  value?: string
+  type?: 'input' | 'textarea'
+  onChange?: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
 }
 
-export const FormContent: React.FC<FormContentProps> = ({ id, name, label, placeholder }) => {
+export const FormContent: React.FC<FormContentProps> = ({ id, name, label, placeholder, value, type, onChange }) => {
   return (
     <div className="mb-4 flex flex-col">
       <label className="mb-2 block text-sm font-bold text-gray-700" htmlFor={id}>
         {label}
       </label>
-      <input
-        className="w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-        type="text"
-        id={id}
-        name={name}
-        placeholder={`Please enter ${placeholder.toLowerCase()}`}
-      />
+      {type === 'input' ? (
+        <input
+          className="w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type="text"
+          id={id}
+          name={name}
+          placeholder={`Please enter ${placeholder.toLowerCase()}`}
+          value={value}
+          onChange={onChange}
+        />
+      ) : (
+        <textarea
+          className="w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          id={id}
+          name={name}
+          placeholder={`Please enter ${placeholder.toLowerCase()}`}
+          value={value}
+          onChange={onChange}
+        />
+      )}
     </div>
   )
 }

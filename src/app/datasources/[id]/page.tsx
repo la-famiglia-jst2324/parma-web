a80be583a61ed016fa797efd089618faf25f0b65
Hamza <chaoukihamza16@gmail.com>
2023-12-05 01:46:53 +0100

    Merge branch with main

--- src/app/datasources/[id]/page.tsx
+++ src/app/datasources/[id]/page.tsx
@@ -15,6 +15,7 @@ import DeleteModal from '@/components/Datasources/DeleteModal'
 import EditInformationModal from '@/components/Datasources/EditInformationModal'
 import { CompaniesTable } from '@/components/Datasources/CompaniesTable'
 import { editDatasource } from '@/utils/datasources/editDatasource'
+import { MainLayout } from '@/components/MainLayout'
 
 // Replace with the URL of the datasource
 const sourceUrl = 'https://www.linkedin.com/feed/'
@@ -115,111 +116,113 @@ export default function DatasourcePage({ params: { id } }: { params: { id: strin
   }
 
   return (
-    <div className="m-6 flex flex-col items-start rounded-lg border-0 bg-white p-5 shadow-md">
-      <div className="mb-3 flex w-full items-center justify-between space-x-4">
-        <div className="mb-2 flex items-center justify-start space-x-4">
-          <GoBackButton />
-          <h1> {data.sourceName} </h1>
-          <div
-            className={`inline-flex items-center rounded-full px-2 py-1 text-sm ${
-              data.isActive ? 'bg-blue-200 text-blue-700' : 'bg-red-200 text-red-700'
-            }`}
-          >
-            <StatusOnlineIcon className="mr-2 h-5 w-5" />
-            {data.isActive ? 'Active' : 'Inactive'}
-          </div>
-        </div>
-        <div className="mr-8 flex items-center justify-end space-x-4">
-          <div>
-            <button
-              className="flex items-center rounded-md border border-slate-500 bg-transparent px-4 py-2 text-slate-500 hover:bg-slate-200 hover:text-gray-700"
-              onClick={handleEditButtonClick}
+    <MainLayout>
+      <div className="m-6 flex flex-col items-start rounded-lg border-0 bg-white p-5 shadow-md">
+        <div className="mb-3 flex w-full items-center justify-between space-x-4">
+          <div className="mb-2 flex items-center justify-start space-x-4">
+            <GoBackButton />
+            <h1> {data.sourceName} </h1>
+            <div
+              className={`inline-flex items-center rounded-full px-2 py-1 text-sm ${
+                data.isActive ? 'bg-blue-200 text-blue-700' : 'bg-red-200 text-red-700'
+              }`}
             >
-              <PencilIcon className="mr-2 h-5 w-5" />
-              Edit Information
-            </button>
-            <EditInformationModal
-              isOpen={isEditModalOpen}
-              handleClose={handleClose}
-              sourceName={sourceName}
-              description={description}
-              url={url}
-              isActive={status}
-              handleSave={async (newName: string, newDescription: string, newUrl: string, newStatus: boolean) => {
-                try {
-                  await handleSave(newName, newStatus, newDescription, newUrl)
-                } catch (error) {
-                  console.error('Failed to save:', error)
-                }
-              }}
-            />
-          </div>
-          <div>
-            {data.isActive ? (
-              <>
-                <Button color="red" onClick={handleDisableButtonClick}>
-                  Disable
-                </Button>
-                <ModalComponent
-                  isOpen={isDisableModalOpen}
-                  handleClose={handleClose}
-                  sourceName={data.sourceName}
-                  description={data.description}
-                  url={sourceUrl}
-                  handleSave={async (newName: string, newDescription: string, newUrl: string, newStatus: boolean) => {
-                    try {
-                      await handleSave(newName, newStatus, newDescription, newUrl)
-                    } catch (error) {
-                      console.error('Failed to save:', error)
-                    }
-                  }}
-                />
-              </>
-            ) : (
-              <Button color="gray" onClick={handleEnableButtonClick}>
-                Enable
-              </Button>
-            )}
-          </div>
-          <div>
-            <button
-              color="blue"
-              className="mr-2 flex items-center bg-transparent text-red-500"
-              onClick={handleDeleteButtonClick}
-            >
-              <TrashIcon className="mr-2 h-5 w-5 text-red-500" />
-              Delete
-            </button>
-            <DeleteModal isOpen={isDeleteModalOpen} handleClose={handleClose} id={data.id.toString()} />
-          </div>
-        </div>
-      </div>
-      <p className="mb-1 ml-9 mr-10 text-base text-gray-700">{data.description}</p>
-      <a
-        href="https://www.linkedin.com/feed/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mb-1 ml-9 text-base text-gray-900 hover:text-blue-600"
-      >
-        {sourceUrl}
-      </a>
-      <TabGroup>
-        <TabList className="mt-8" variant="solid">
-          <Tab icon={OfficeBuildingIcon}>Companies Monitored</Tab>
-          <Tab icon={PresentationChartLineIcon}>Datasource Health</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <CompaniesTable />
-          </TabPanel>
-          <TabPanel>
-            <div className="flex flex-col items-center justify-center">
-              <h1 className="text-2xl font-bold">No data available</h1>
-              <p className="text-gray-500">No data has been collected yet</p>
+              <StatusOnlineIcon className="mr-2 h-5 w-5" />
+              {data.isActive ? 'Active' : 'Inactive'}
             </div>
-          </TabPanel>
-        </TabPanels>
-      </TabGroup>
-    </div>
+          </div>
+          <div className="mr-8 flex items-center justify-end space-x-4">
+            <div>
+              <button
+                className="flex items-center rounded-md border border-slate-500 bg-transparent px-4 py-2 text-slate-500 hover:bg-slate-200 hover:text-gray-700"
+                onClick={handleEditButtonClick}
+              >
+                <PencilIcon className="mr-2 h-5 w-5" />
+                Edit Information
+              </button>
+              <EditInformationModal
+                isOpen={isEditModalOpen}
+                handleClose={handleClose}
+                sourceName={sourceName}
+                description={description}
+                url={url}
+                isActive={status}
+                handleSave={async (newName: string, newDescription: string, newUrl: string, newStatus: boolean) => {
+                  try {
+                    await handleSave(newName, newStatus, newDescription, newUrl)
+                  } catch (error) {
+                    console.error('Failed to save:', error)
+                  }
+                }}
+              />
+            </div>
+            <div>
+              {data.isActive ? (
+                <>
+                  <Button color="red" onClick={handleDisableButtonClick}>
+                    Disable
+                  </Button>
+                  <ModalComponent
+                    isOpen={isDisableModalOpen}
+                    handleClose={handleClose}
+                    sourceName={data.sourceName}
+                    description={data.description}
+                    url={sourceUrl}
+                    handleSave={async (newName: string, newDescription: string, newUrl: string, newStatus: boolean) => {
+                      try {
+                        await handleSave(newName, newStatus, newDescription, newUrl)
+                      } catch (error) {
+                        console.error('Failed to save:', error)
+                      }
+                    }}
+                  />
+                </>
+              ) : (
+                <Button color="gray" onClick={handleEnableButtonClick}>
+                  Enable
+                </Button>
+              )}
+            </div>
+            <div>
+              <button
+                color="blue"
+                className="mr-2 flex items-center bg-transparent text-red-500"
+                onClick={handleDeleteButtonClick}
+              >
+                <TrashIcon className="mr-2 h-5 w-5 text-red-500" />
+                Delete
+              </button>
+              <DeleteModal isOpen={isDeleteModalOpen} handleClose={handleClose} id={data.id.toString()} />
+            </div>
+          </div>
+        </div>
+        <p className="mb-1 ml-9 mr-10 text-base text-gray-700">{data.description}</p>
+        <a
+          href="https://www.linkedin.com/feed/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mb-1 ml-9 text-base text-gray-900 hover:text-blue-600"
+        >
+          {sourceUrl}
+        </a>
+        <TabGroup>
+          <TabList className="mt-8" variant="solid">
+            <Tab icon={OfficeBuildingIcon}>Companies Monitored</Tab>
+            <Tab icon={PresentationChartLineIcon}>Datasource Health</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <CompaniesTable />
+            </TabPanel>
+            <TabPanel>
+              <div className="flex flex-col items-center justify-center">
+                <h1 className="text-2xl font-bold">No data available</h1>
+                <p className="text-gray-500">No data has been collected yet</p>
+              </div>
+            </TabPanel>
+          </TabPanels>
+        </TabGroup>
+      </div>
+    </MainLayout>
   )
 }
--- src/app/datasources/add-datasource/page.tsx
+++ src/app/datasources/add-datasource/page.tsx
@@ -6,6 +6,7 @@ import { CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/solid'
 import GoBackButton from '@/components/Datasources/GoBackButton'
 import { FormContent } from '@/components/FormContent'
 import { Frequency } from '@/types/datasource'
+import { MainLayout } from '@/components/MainLayout'
 
 export default function CreateDatasourcePage() {
   const [name, setName] = useState('')
@@ -60,71 +61,73 @@ export default function CreateDatasourcePage() {
 
   return (
     <>
-      <div className="mx-6 h-screen pt-12">
-        <div className="mx-auto max-w-screen-xl rounded-lg border-0 bg-white p-6 shadow-md">
-          <div className="mb-3 flex items-center justify-start space-x-4">
-            <GoBackButton />
-            <h1 className="mb-2 text-2xl font-bold">Create Datasource</h1>
+      <MainLayout>
+        <div className="mx-6 h-screen pt-12">
+          <div className="mx-auto max-w-screen-xl rounded-lg border-0 bg-white p-6 shadow-md">
+            <div className="mb-3 flex items-center justify-start space-x-4">
+              <GoBackButton />
+              <h1 className="mb-2 text-2xl font-bold">Create Datasource</h1>
+            </div>
+            <p className="mb-4">Create a datasource by providing the name, URL and description.</p>
+            <form role="form" data-testid="create-datasource-form" onSubmit={createDatasource}>
+              <FormContent
+                id="name"
+                name="name"
+                label="Datasource Name"
+                placeholder="Please enter datasource name"
+                value={name}
+                type="input"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <FormContent
+                id="description"
+                name="description"
+                label="Datasource Description"
+                placeholder="Please enter datasource description"
+                value={description}
+                type="textarea"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <div className="mb-4 flex flex-col">
+                <label className="mb-2 block text-sm font-bold text-gray-700">Frequency</label>
+                <Select value={defaultFrequency} onValueChange={setDefaultFrequency}>
+                  <SelectItem value="DAILY">Daily</SelectItem>
+                  <SelectItem value="WEEKLY">Weekly</SelectItem>
+                </Select>
+              </div>
+              <FormContent
+                id="url"
+                name="url"
+                label="Datasource URL"
+                placeholder="Please enter datasource URL"
+                value={url}
+                type="input"
+                onChange={(e) => setUrl(e.target.value)}
+              />
+              <div>
+                <button
+                  className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                  type="submit"
+                >
+                  Create Datasource
+                </button>
+              </div>
+            </form>
           </div>
-          <p className="mb-4">Create a datasource by providing the name, URL and description.</p>
-          <form role="form" data-testid="create-datasource-form" onSubmit={createDatasource}>
-            <FormContent
-              id="name"
-              name="name"
-              label="Datasource Name"
-              placeholder="Please enter datasource name"
-              value={name}
-              type="input"
-              onChange={(e) => setName(e.target.value)}
-            />
-            <FormContent
-              id="description"
-              name="description"
-              label="Datasource Description"
-              placeholder="Please enter datasource description"
-              value={description}
-              type="textarea"
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <div className="mb-4 flex flex-col">
-              <label className="mb-2 block text-sm font-bold text-gray-700">Frequency</label>
-              <Select value={defaultFrequency} onValueChange={setDefaultFrequency}>
-                <SelectItem value="DAILY">Daily</SelectItem>
-                <SelectItem value="WEEKLY">Weekly</SelectItem>
-              </Select>
-            </div>
-            <FormContent
-              id="url"
-              name="url"
-              label="Datasource URL"
-              placeholder="Please enter datasource URL"
-              value={url}
-              type="input"
-              onChange={(e) => setUrl(e.target.value)}
-            />
-            <div>
-              <button
-                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-                type="submit"
-              >
-                Create Datasource
-              </button>
-            </div>
-          </form>
+          <div className="fixed bottom-0 right-0 m-6 w-96">
+            {showSuccess && (
+              <Callout title="Datasource created successfully" icon={CheckCircleIcon} color="teal">
+                You have successfully created a datasource. This datasource will be added to the list of datasources.
+              </Callout>
+            )}
+            {showError && (
+              <Callout title="Error creating datasource" icon={ExclamationCircleIcon} color="red">
+                There was an error creating the datasource. Please try again.
+              </Callout>
+            )}
+          </div>
         </div>
-        <div className="fixed bottom-0 right-0 m-6 w-96">
-          {showSuccess && (
-            <Callout title="Datasource created successfully" icon={CheckCircleIcon} color="teal">
-              You have successfully created a datasource. This datasource will be added to the list of datasources.
-            </Callout>
-          )}
-          {showError && (
-            <Callout title="Error creating datasource" icon={ExclamationCircleIcon} color="red">
-              There was an error creating the datasource. Please try again.
-            </Callout>
-          )}
-        </div>
-      </div>
+      </MainLayout>
     </>
   )
 }

0cd8dd703d7aa5abdbc09b2d403dd9a0e473b73d
Saiyam Jain <ge63req@mytum.com>
2023-12-12 20:47:11 +0100

    feat: Recent changes profile, settings page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,10 @@
 import React, { useState } from 'react'
 import { Button } from '@tremor/react'
 import Image from 'next/image'
+import profilePic from '@/../../public/Default_pfp.jpg'
+import { FormContent } from '@/components/FormContent'
+import AuthCheck from '@/components/Authentication/AuthCheck'
+import { MainLayout } from '@/components/MainLayout'
 
 const ProfilePage: React.FC = () => {
   const [fullName, setFullName] = useState('')
@@ -10,90 +14,158 @@ const ProfilePage: React.FC = () => {
   const [newPassword, setNewPassword] = useState('')
   const [repeatPassword, setRepeatPassword] = useState('')
 
-  const handleFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    // Handle form submission logic here
+  const saveProfileData = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    // Create an object with the form data
+    const formData = {
+      fullName,
+      email,
+      oldPassword,
+      newPassword,
+      repeatPassword
+    }
+
+    // Validate the form data here (if necessary)
+
+    // Call the REST API endpoint
+    try {
+      const response = await fetch('/api/profile', {
+        method: 'PUT',
+        cache: 'no-cache',
+        body: JSON.stringify(formData)
+      })
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+
+      // Handle the response here
+      const data = await response.json()
+      console.log('Profile updated successfully:', data)
+      // Additional actions based on the response (e.g., redirect, show a message)
+    } catch (error) {
+      console.error('Error updating profile:', error)
+      // Handle errors here (e.g., show error message to the user)
+    }
+  }
+  const uploadFile = async (file: File) => {
+    const formData = new FormData()
+    formData.append('file', file)
+
+    try {
+      // Post the form data to your API route for file upload
+      // const response = ''
+    } catch (error) {
+      console.error('Error uploading file:', error)
+    }
+  }
+  // Rest api still needs to be implemented
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      // Get the first file if multiple files are selected (assuming single file upload)
+      const file = event.target.files[0]
+      // setSelectedFile(file); // Save the file to the component's state
+
+      // Here you might want to update the UI to show the file name or preview the image
+      // ...
+
+      // If you want to automatically upload after file selection:
+      uploadFile(file)
+    }
   }
 
   return (
-    <div className="profile-form m-3 flex flex-col items-start rounded-lg border-0 bg-white p-3 shadow-md">
-      <div className=" items-center justify-start ">
-        <div className="mb-3 items-center justify-start space-x-7">
-          <h1 className="title py-2 pl-2 text-3xl font-bold">Edit Profile</h1>
+    <MainLayout>
+      <div className="profile-form m-3 flex flex-col items-start rounded-lg border-0 bg-white p-3 shadow-md">
+        <div className=" items-center justify-start ">
+          <div className="mb-3 items-center justify-start space-x-7">
+            <h1 className="title py-2 pl-2 text-3xl font-bold">Edit Profile</h1>
 
-          <div className="flex">
-            <div className="profile-image-container">
-              <Image className="profile-image" src="path/to/your/img" width={500} height={500} alt="Profile" />
+            <div className="flex">
+              <div className="profile-image-container pl-10 pt-12">
+                <div className="pl-10">
+                  <Image className="profile-image" src={profilePic} width={500} height={500} alt="Profile" />
+                </div>
 
-              <Button className="profile-pic-button">New Profile Picture</Button>
-            </div>
-            <div style={{ paddingLeft: '300px', width: '800px' }}>
-              <form className="form-fields" onSubmit={handleFormSubmit}>
-                <div className="form-field">
-                  <label htmlFor="fullName">Full Name</label>
+                <div>
                   <input
-                    className="input-fullname"
-                    type="text"
-                    placeholder="Full Name"
-                    value={fullName}
+                    type="file"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                    id="profile-pic-input"
+                    style={{ display: 'none' }}
+                  />
+                  <Button
+                    className="profile-pic-button"
+                    onClick={() => document.getElementById('profile-pic-input')?.click()}
+                  >
+                    New Profile Picture
+                  </Button>
+                </div>
+              </div>
+              <div className="text-bold w-[800px] pl-72  pt-10">
+                <form role="form" data-testid="create-profile-form" onSubmit={saveProfileData}>
+                  <FormContent
+                    id="Full Name"
+                    name="Full Name"
+                    label="Full Name"
+                    placeholder="your full name here"
+                    type="input"
                     onChange={(e) => setFullName(e.target.value)}
                   />
-                </div>
-
-                <div className="form-field">
-                  <label htmlFor="email">Email</label>
-                  <input
-                    className="input-email"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
+                  <FormContent
+                    id="Email"
+                    name="Email"
+                    label="Email"
+                    placeholder="your email here"
+                    type="input"
                     onChange={(e) => setEmail(e.target.value)}
                   />
-                </div>
-
-                <div className="form-field">
-                  <label htmlFor="oldPassword">Old Password</label>
-                  <input
-                    className="input-old-password"
-                    type="password"
-                    placeholder="Old Password"
-                    value={oldPassword}
+                  <FormContent
+                    id="password"
+                    name="Old Password"
+                    label="Old Password"
+                    placeholder="your old password here"
+                    type="input"
                     onChange={(e) => setOldPassword(e.target.value)}
                   />
-                </div>
-
-                <div className="form-field">
-                  <label htmlFor="newPassword">New Password</label>
-                  <input
-                    className="input-new-password"
-                    type="password"
-                    placeholder="New Password"
-                    value={newPassword}
+                  <FormContent
+                    id="Password"
+                    name="New Password"
+                    label="New Password"
+                    placeholder="your new password here"
+                    type="input"
                     onChange={(e) => setNewPassword(e.target.value)}
                   />
-                </div>
-
-                <div className="form-field">
-                  <label htmlFor="repeatPassword">Repeat New Password</label>
-                  <input
-                    className="input-repeat-password"
-                    type="password"
-                    placeholder="Repeat New Password"
-                    value={repeatPassword}
+                  <FormContent
+                    id="password"
+                    name="Repeat New Password"
+                    label="Repeat New Password"
+                    placeholder="your new password here"
+                    type="input"
                     onChange={(e) => setRepeatPassword(e.target.value)}
                   />
-                </div>
-
-                <Button className="save-button" type="submit">
-                  Save Changes
-                </Button>
-              </form>
+                  <div>
+                    <button
+                      className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                      type="submit"
+                    >
+                      Save Changes
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </MainLayout>
   )
 }
 
-export default ProfilePage
+export default AuthCheck(ProfilePage)
+
+// function setSelectedFile(file: File) {
+//   throw new Error('Function not implemented.')
+// }
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import { Button, MultiSelect, MultiSelectItem } from '@tremor/react'
+import { Button } from '@tremor/react'
 import type { Company } from '@/types/companies'
+import type { Bucket } from '@/types/bucket'
+import SelectSection from '@/components/Settings/SelectSection'
+import ApiKeyConfiguration from '@/components/Settings/ApiKeyConfiguration'
+import { MainLayout } from '@/components/MainLayout'
+import AuthCheck from '@/components/Authentication/AuthCheck'
 
 async function getSubscribedCompanies() {
   try {
-    const res = await fetch('/api/companies/subscribed-companies', {
+    const res = await fetch('/api/company', {
       method: 'GET',
       cache: 'no-cache'
     })
@@ -20,44 +25,43 @@ async function getSubscribedCompanies() {
     console.log('An error has occurred: ', error)
   }
 }
-// async function getBuckets() {
-//   try {
-//     const res = await fetch('/api/companies/subscribed-companies', {
-//       method: 'GET',
-//       cache: 'no-cache'
-//     })
-//     if (!res.ok) {
-//       console.log('Response status:', res.status)
-//       throw new Error('HTTP response was not OK')
-//     }
-//     const json = await res.json()
-//     return json
-//   } catch (error) {
-//     console.log('An error has occurred: ', error)
-//   }
-// }
 
-// async function getChannels() {
-//   try {
-//     const res = await fetch('/api/companies/subscribed-companies', {
-//       method: 'GET',
-//       cache: 'no-cache'
-//     })
-//     if (!res.ok) {
-//       console.log('Response status:', res.status)
-//       throw new Error('HTTP response was not OK')
-//     }
-//     const json = await res.json()
-//     return json
-//   } catch (error) {
-//     console.log('An error has occurred: ', error)
-//   }
-// }
-export default function SettingsPage() {
+async function getBuckets() {
+  try {
+    const res = await fetch('/api/bucket', {
+      method: 'GET',
+      cache: 'no-cache'
+    })
+    if (!res.ok) {
+      console.log('Response status:', res.status)
+      throw new Error('HTTP response was not OK')
+    }
+    const json = await res.json()
+    console.log(json)
+    return json
+  } catch (error) {
+    console.log('An error has occurred: ', error)
+  }
+}
+
+function SettingsPage() {
   const [selectedCompanies, setSelectedCompanies] = useState<string[]>([])
   const [subscribedCompanies, setSubscribedCompanies] = useState<Company[]>([])
-  const uniqueCompanies = Array.from(new Set(subscribedCompanies.map((company) => company.name)))
+  const [subscribedBuckets, setSubscribedBuckets] = useState<Bucket[]>([])
+  const [selectedBuckets, setSelectedBuckets] = useState<string[]>([])
+  const [selectedChannels, setSelectedChannels] = useState<string[]>([])
+
+  const uniqueCompanies = Array.from(new Set(subscribedCompanies?.map((company) => company.name)))
+  const uniqueBuckets = Array.from(new Set(subscribedBuckets?.map((company) => company.title)))
+  const channels = ['Email', 'Slack']
+
+  // tbd
+  const handleConfigureSlack = () => {}
+  const handleConfigureAffinity = () => {}
+
   selectedCompanies.map((company) => console.log(company))
+  selectedBuckets.map((bucket) => console.log(bucket))
+  selectedChannels.map((channel) => console.log(channel))
   useEffect(() => {
     getSubscribedCompanies()
       .then((res) => setSubscribedCompanies(res))
@@ -65,115 +69,69 @@ export default function SettingsPage() {
         console.error('Failed to fetch subscribed companies:', error)
       })
   }, [])
+  useEffect(() => {
+    getBuckets()
+      .then((res) => setSubscribedBuckets(res))
+      .catch((error) => {
+        console.error('Failed to fetch buckets', error)
+      })
+  }, [])
+
+  function saveChanges(): React.MouseEventHandler<HTMLDivElement> | undefined {
+    throw new Error('Function not implemented.')
+  }
 
   return (
-    <div className="bg-white">
+    <MainLayout>
       <div className="m-3 flex flex-col items-start rounded-lg border-0 bg-white p-3 shadow-md">
         <div className="mb-3 items-center justify-start space-x-6">
           <div className="mb-3  items-center justify-start space-x-3 ">
-            <h1 className="py-2 pl-2 text-3xl font-bold ">Preferences</h1>
+            <h1 className="py-2 pl-2 text-3xl font-bold text-slate-700">Preferences</h1>
 
             <div className="p-8">
               <h2 className="text-xl font-bold ">Report Configuration</h2>
 
-              <div className="pb-1 pt-6 font-bold">
-                <h3>Select Companies</h3>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="flex">Select companies to receive weekly reports on</h4>
-                </div>
-                <div className="absolute right-8 w-56 pb-3">
-                  <MultiSelect
-                    placeholder="companies"
-                    onValueChange={(values) => setSelectedCompanies(values)}
-                    className=""
-                  >
-                    {uniqueCompanies.map((companyName, index) => (
-                      <MultiSelectItem key={index} value={companyName}>
-                        {companyName}
-                      </MultiSelectItem>
-                    ))}
-                  </MultiSelect>
-                </div>
-              </div>
-
-              <div className="pb-1 pt-6 font-bold">
-                <h3>Select Buckets</h3>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="flex">Select buckets to receive weekly reports on</h4>
-                </div>
-                <div className="absolute right-8 w-56 pb-3">
-                  <MultiSelect
-                    placeholder="buckets"
-                    onValueChange={(values) => setSelectedCompanies(values)}
-                    className=""
-                  >
-                    {uniqueCompanies.map((companyName, index) => (
-                      <MultiSelectItem key={index} value={companyName}>
-                        {companyName}
-                      </MultiSelectItem>
-                    ))}
-                  </MultiSelect>
-                </div>
-              </div>
-
-              <div className="pb-1 pt-6 font-bold">
-                <h3>Select Channels</h3>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="text-align: left; flex">Select where you want to receive the report</h4>
-                </div>
-                <div className="absolute right-8 w-56 pb-3">
-                  <MultiSelect
-                    placeholder="channels"
-                    onValueChange={(values) => setSelectedCompanies(values)}
-                    className=""
-                  >
-                    {uniqueCompanies.map((companyName, index) => (
-                      <MultiSelectItem key={index} value={companyName}>
-                        {companyName}
-                      </MultiSelectItem>
-                    ))}
-                  </MultiSelect>
-                </div>
-              </div>
+              <SelectSection
+                title="Select Companies"
+                description="Select companies to receive weekly reports on"
+                placeholder="companies"
+                options={uniqueCompanies}
+                onValueChange={setSelectedCompanies}
+              />
+              <SelectSection
+                title="Select Buckets"
+                description="Select buckets to receive weekly reports on"
+                placeholder="buckets"
+                options={uniqueBuckets}
+                onValueChange={setSelectedBuckets}
+              />
+              <SelectSection
+                title="Select Channels"
+                description="Select channels for receiving reports"
+                placeholder="channels"
+                options={channels}
+                onValueChange={setSelectedChannels}
+                isMultiSelect={false}
+              />
             </div>
 
+            <div className="pl-8" onClick={saveChanges}>
+              <Button>Save changes</Button>
+            </div>
             <div className="w-full rounded-b border-b border-gray-200 p-2"></div>
 
             <div className="p-8">
               <h2 className="text-xl font-bold ">API Keys</h2>
-              <div className="pt-6 font-bold ">
-                <h3>Slack</h3>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="text-align: left; flex">Configure API key for slack </h4>
-                </div>
-                <div className="absolute right-8 w-[180px] p-2">
-                  <Button>Configure</Button>
-                </div>
-              </div>
 
-              <div className="pt-6 font-bold">
-                <h3>Affinity</h3>
-              </div>
-              <div className="flex">
-                <div>
-                  <h4 className="flex">Configure API key for Affinity</h4>
-                </div>
-                <div className="absolute right-8 w-[180px] p-2">
-                  <Button>Configure</Button>
-                </div>
-              </div>
+              <ApiKeyConfiguration serviceName="Slack" onConfigure={handleConfigureSlack} />
+
+              <ApiKeyConfiguration serviceName="Affinity" onConfigure={handleConfigureAffinity} />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </MainLayout>
   )
 }
+
+export default AuthCheck(SettingsPage)

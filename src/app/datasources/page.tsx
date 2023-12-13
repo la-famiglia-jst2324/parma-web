13466e234c4cef7051a42c18cd387f8677771efa
Hamza <70699922+HamzaChx@users.noreply.github.com>
2023-12-13 13:58:41 +0100

    feat: Adding Pagination to Datasource Table (#80)
    Co-authored-by: GitHub Actions <action@github.com>

--- src/app/datasources/page.tsx
+++ src/app/datasources/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import type { DataSource } from '@prisma/client'
 import Table from '../../components/Datasources/Table'
-import type Datasource from '@/types/datasource'
 import { MainLayout } from '@/components/MainLayout'
 import AuthCheck from '@/components/Authentication/AuthCheck'
 import CustomButton from '@/components/BlueButton'
 
-async function getDatasources() {
+async function getDatasources(page: number, size: number) {
   try {
-    const res = await fetch('/api/dataSources', {
+    const res = await fetch(`/api/dataSources?page=${page}&size=${size}`, {
       method: 'GET',
       cache: 'no-cache'
     })
@@ -18,22 +18,38 @@ async function getDatasources() {
       throw new Error('HTTP response was not OK')
     }
     const json = await res.json()
-    return json
+    return {
+      datasources: json.datasources,
+      pagination: json.pagination
+    }
   } catch (error) {
-    console.log('An error has occurred: ', error)
+    console.error('An error has occurred: ', error)
+    throw error
   }
 }
 
 function DatasourcesPage() {
-  const [data, setData] = useState<Datasource[]>([])
+  const [data, setData] = useState<DataSource[] | null>(null)
+  const [pagination, setPagination] = useState({ currentPage: 1, pageSize: 10, totalPages: 0, totalCount: 0 })
 
   useEffect(() => {
-    getDatasources()
-      .then(setData)
+    getDatasources(pagination.currentPage, pagination.pageSize)
+      .then((response) => {
+        setData(response.datasources)
+        setPagination(response.pagination)
+      })
       .catch((error) => {
         console.error('Failed to fetch datasources:', error)
       })
-  }, [])
+  }, [pagination.currentPage, pagination.pageSize])
+
+  const handlePageChange = (newPage: number) => {
+    setPagination((prevState) => ({ ...prevState, currentPage: newPage }))
+  }
+
+  const handleItemsPerPageChange = (newSize: number) => {
+    setPagination((prevState) => ({ ...prevState, pageSize: newSize }))
+  }
 
   const router = useRouter()
   const navigateToCreate = () => {
@@ -44,26 +60,31 @@ function DatasourcesPage() {
     <>
       <MainLayout>
         <div className="relative m-5 flex min-h-screen w-auto flex-col justify-start rounded-md bg-white shadow-lg">
-          <div className="flex items-center justify-between p-4">
+          <div className="flex items-center justify-between p-6">
             <div className="mb-4 flex items-center justify-start space-x-4">
               <h1 className="m-4 text-4xl text-black">Datasources</h1>
             </div>
-            <div className="m-4">
+            <div className="m-5">
               <CustomButton text="Create Datasource" onClick={navigateToCreate} />
             </div>
           </div>
-          <div className="p-8">
-            {data ? (
-              <div className="mx-4 rounded-lg border-0 bg-white shadow-md">
-                <div>
-                  <Table data={data} />
-                </div>
+          <div className="mb-8">
+            <div className="mx-auto max-w-6xl overflow-auto rounded-lg border-0 bg-white shadow-md">
+              <div className="w-full">
+                {data ? (
+                  <Table
+                    initialData={data}
+                    pagination={pagination}
+                    onPageChange={handlePageChange}
+                    onItemsPerPageChange={handleItemsPerPageChange}
+                  />
+                ) : (
+                  <p className="text-lg font-bold text-gray-700">
+                    No datasources available yet. Start by creating one.
+                  </p>
+                )}
               </div>
-            ) : (
-              <p className="text-lg font-semibold text-gray-600">
-                No datasources available yet. Start by creating one.
-              </p>
-            )}
+            </div>
           </div>
         </div>
       </MainLayout>

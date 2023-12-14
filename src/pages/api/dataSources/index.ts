4948fcd7ded6d464b3b96d9de0f6901304a08abe
onur-oruc <honouroruc@gmail.com>
2023-12-14 03:58:34 +0100

    feat: add data source handshaking on data source creation

--- src/pages/api/dataSources/index.ts
+++ src/pages/api/dataSources/index.ts
@@ -1,6 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { getAllDataSources, createDataSource } from '@/api/db/services/dataSourceService'
+import type { Frequency } from '@prisma/client'
+import { getAllDataSources, createDataSource, updateDataSource } from '@/api/db/services/dataSourceService'
+
+const frequencyMapping: { [key: string]: Frequency | undefined } = {
+  weekly: 'WEEKLY',
+  daily: 'DAILY'
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req
@@ -24,8 +30,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       try {
         // Create a new data source
         const newDataSource = await createDataSource(req.body)
-        if (newDataSource) {
-          res.status(201).json(newDataSource)
+
+        const analyticsUrl = process.env.PARMA_ANALYTICS_BASE_URL
+        console.log('analytics url: ' + analyticsUrl)
+        if (!analyticsUrl) {
+          throw new Error('PARMA_ANALYTICS_URL is not defined in the environment.')
+        }
+        const { id, invocationEndpoint } = newDataSource
+        // send a request to parma-analytics to perform handshake
+        const handshakeUrl = new URL('/handshake', analyticsUrl)
+        handshakeUrl.searchParams.append('invocation_endpoint', invocationEndpoint)
+        handshakeUrl.searchParams.append('data_source_id', id.toString())
+
+        const handshakeResponse = await fetch(handshakeUrl.href)
+        // Check the handshake response
+        if (!handshakeResponse.ok) {
+          throw new Error('Handshake for the data source failed.')
+        }
+        // update frequency of the data source after the handshake
+        const handshakeData = await handshakeResponse.json()
+        const frequency = handshakeData.frequency
+        // convert frequency to the format used in the database
+        const frequencyInDB: Frequency | undefined = frequencyMapping[frequency]
+
+        if (!frequencyInDB) {
+          await updateDataSource(id, { isActive: false, healthStatus: 'DOWN' })
+          throw new Error(`The frequency '${frequency}' is not recognized.`)
+        }
+
+        const updatedDataSource = await updateDataSource(id, {
+          defaultFrequency: frequency,
+          isActive: true,
+          healthStatus: 'UP'
+        })
+
+        if (updatedDataSource) {
+          res.status(201).json(updatedDataSource)
         } else res.status(400).json({ error: 'Invalid request parameters' })
       } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' })

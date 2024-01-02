dba852e3f8868ff855ef2b938d69e70d39a63216
alouiii <achref.aloui73@gmail.com>
2024-01-02 23:27:19 +0100

    precommit changes

--- src/pages/api/lib/utils/firebaseStorage.ts
+++ src/pages/api/lib/utils/firebaseStorage.ts
@@ -58,27 +58,27 @@ async function deleteFileFromFirebaseStorage(filePath: string) {
  * Asynchronously fetches raw data for a specific company from all datasources in Firestore.
  *
  * @param {string} companyId - The ID of the company for which to fetch the data.
- * @returns {Promise<Record<string, admin.firestore.DocumentData[]>>} - A promise that resolves to an object. 
- * Each key in the object is a datasource name, and the value is an array of documents from that datasource. 
+ * @returns {Promise<Record<string, admin.firestore.DocumentData[]>>} - A promise that resolves to an object.
+ * Each key in the object is a datasource name, and the value is an array of documents from that datasource.
  * Each document is represented as an object where the keys are the field names and the values are the field values.
  */
 async function readRawDataByAllDatasources(companyId: string): Promise<Record<string, admin.firestore.DocumentData[]>> {
-  const db = admin.firestore();
-  const datasourcesSnapshot = await db.collection('parma/mining/datasource').listDocuments();
+  const db = admin.firestore()
+  const datasourcesSnapshot = await db.collection('parma/mining/datasource').listDocuments()
 
-  const response: Record<string, admin.firestore.DocumentData[]> = {};
+  const response: Record<string, admin.firestore.DocumentData[]> = {}
   for (const doc of datasourcesSnapshot) {
-    const datasource = doc.id;
-    const collectionPath = `parma/mining/datasource/${datasource}/raw_data`;
-    const snapshot = await db.collection(collectionPath).where('company_id', '==', companyId).get();
+    const datasource = doc.id
+    const collectionPath = `parma/mining/datasource/${datasource}/raw_data`
+    const snapshot = await db.collection(collectionPath).where('company_id', '==', companyId).get()
 
-    response[datasource] = [];
-    snapshot.forEach(doc => {
-      response[datasource].push(doc.data());
-    });
+    response[datasource] = []
+    snapshot.forEach((doc) => {
+      response[datasource].push(doc.data())
+    })
   }
 
-  return response;
+  return response
 }
 
 export { uploadFileToFirebase, generateFileUrl, deleteFileFromFirebaseStorage, readRawDataByAllDatasources }

f4038dfca0899c4dbd7c16ae336249d9b1d10a33
Saiyam Jain <ge63req@mytum.com>
2024-01-10 18:52:37 +0100

    feat: Refine Datasource page

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { UserIcon, Cog8ToothIcon, RocketLaunchIcon } from '@heroicons/react/20/solid'
+import { LogOutIcon } from 'lucide-react'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from './ui/dropdown-menu'
@@ -35,8 +35,6 @@ const UserNav: React.FC<UserNavProps> = () => {
   }
 
   const initials = user === null || user === 'loading' ? '' : extractInitials(user?.displayName)
-  const displayName = user === null || user === 'loading' ? '' : user?.displayName
-  const email = user === null || user === 'loading' ? '' : user?.email
 
   const handleLogoutClick = async () => {
     if (user) {
@@ -49,30 +47,45 @@ const UserNav: React.FC<UserNavProps> = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
+          <Avatar className="h-8 w-8 transition duration-500 ease-in hover:cursor-pointer hover:shadow-[0_0_15px_15px_rgba(63,55,201,0.5)]">
             <AvatarImage src={photoURL || ''} />
             <AvatarFallback>{initials}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="start" forceMount>
-        <DropdownMenuLabel className="font-normal">
+      <DropdownMenuContent className="w-40" align="start" forceMount>
+        {/* <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">{displayName}</p>
             <p className="text-xs leading-none text-muted-foreground">{email}</p>
           </div>
         </DropdownMenuLabel>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator /> */}
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <Link href="/profile">Profile</Link>
+          <DropdownMenuItem className="hover:cursor-pointer" onClick={() => router.push('/profile')}>
+            <div className="flex">
+              <UserIcon className="ml-2 mr-4 h-6 w-4 text-[#374151]" />
+              Profile
+            </div>
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="/settings">Settings</Link>
+          <DropdownMenuItem className="hover:cursor-pointer" onClick={() => router.push('/datasources')}>
+            <div className="flex">
+              <RocketLaunchIcon className="ml-2 mr-4 h-6 w-4 text-[#374151]" />
+              Datasources
+            </div>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="hover:cursor-pointer" onClick={() => router.push('/settings')}>
+            <div className="flex">
+              <Cog8ToothIcon className="ml-2 mr-4 h-6 w-4 text-[#374151]" />
+              Settings
+            </div>
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={handleLogoutClick}>Log out</DropdownMenuItem>
+        <DropdownMenuItem className="hover:cursor-pointer" onClick={handleLogoutClick}>
+          <LogOutIcon className="ml-2 mr-4 h-6 w-4 text-[#374151]" />
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )

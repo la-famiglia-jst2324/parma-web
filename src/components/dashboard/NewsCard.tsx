f9308f47b64dd7ee3bc2f5253fa975ff0d84ff7e
Hamza <70699922+HamzaChx@users.noreply.github.com>
2024-01-13 18:49:20 +0100

    feat: News Feed Redesign (#154)
    Co-authored-by: GitHub Actions <action@github.com>

--- src/components/dashboard/NewsCard.tsx
+++ src/components/dashboard/NewsCard.tsx
@@ -1,13 +1,14 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
+import Link from 'next/link'
 import type NewsItem from '@/types/news'
 import { CardTitle, CardDescription, CardHeader, CardContent, CardFooter, Card } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
 
 interface NewsCardProps extends NewsItem {}
 
 const NewsCard: React.FC<NewsCardProps> = ({
   title,
+  id,
   companyName,
   datasourceName,
   bucketName,
@@ -16,78 +17,48 @@ const NewsCard: React.FC<NewsCardProps> = ({
   measureValue,
   description
 }) => {
-  const [isAccordionOpen, setIsAccordionOpen] = useState(false)
-  const contentRef = useRef(null)
-
-  const toggleAccordion = () => {
-    setIsAccordionOpen(!isAccordionOpen)
-  }
-
-  const openClasses = 'max-h-96 overflow-auto transition-max-height duration-700 ease-in-out'
-  const closedClasses = 'max-h-0 overflow-hidden transition-max-height duration-700 ease-out'
-
   return (
-    <Card className="border border-gray-700 bg-gray-900 transition-all duration-300 ease-in-out hover:border-gray-400 hover:bg-slate-900">
-      <CardHeader className="p-4">
-        <CardTitle className="text-lg font-semibold text-white">{title}</CardTitle>
-        <CardDescription className="text-gray-400">{datasourceName}</CardDescription>
-      </CardHeader>
-      <CardContent className="p-4">
-        <div className="grid grid-cols-2 items-center gap-4 text-sm text-gray-300 md:grid-cols-4">
-          <div className="flex flex-col items-center space-y-2">
-            <Badge className="rounded-full bg-sky-600 px-3 py-1 text-xs text-white transition-colors duration-300 ease-in-out hover:bg-blue-700">
-              Notification Date
+    <Card className="rounded-lg border border-gray-700 bg-gray-900 shadow-lg transition-all duration-300 ease-in-out hover:border-gray-400 hover:bg-slate-800 hover:shadow-2xl">
+      <CardHeader className="pb-3 pt-6">
+        <div className="flex items-center justify-between space-x-4">
+          <div className="flex-1">
+            <Link href={`/companies/${id}`}>
+              <CardTitle className="text-xl font-semibold text-gray-200 transition-colors duration-200 hover:text-white">
+                {title}
+              </CardTitle>
+            </Link>
+          </div>
+          <div className="shrink-0">
+            <Badge className="bg-indigo-600 px-3 py-1 text-white transition-all duration-200 hover:bg-indigo-500">
+              {measureName}: {measureValue}
             </Badge>
-            <div className="my-2 w-full border-b border-transparent"></div>
+          </div>
+        </div>
+        <CardDescription className="mt-2 text-gray-400">{datasourceName}</CardDescription>
+      </CardHeader>
+      <CardContent className="pb-6 pt-3">
+        <p className="text-gray-300">{description}</p>
+      </CardContent>
+      <CardFooter className="rounded-b-lg bg-slate-800 px-6 py-3 transition-colors duration-200 hover:bg-slate-700">
+        <div className="flex space-x-4 text-sm text-gray-300">
+          <div className="flex items-center">
+            <span className="mr-1 text-gray-400">Company:</span>
+            <span className="text-white">{companyName}</span>
+          </div>
+          <div className="flex items-center">
+            <span className="mr-1 text-gray-400">Notification Date:</span>
             <span className="text-white">{timestamp}</span>
           </div>
-          <div className="flex flex-col items-center space-y-2">
-            <Badge className="rounded-full bg-sky-600 px-3 py-1 text-xs text-white transition-colors duration-300 ease-in-out hover:bg-blue-700">
-              {measureName}
-            </Badge>
-            <div className="my-2 w-full border-b border-transparent"></div>
-            <span className="text-white">{measureValue}</span>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <Badge className="rounded-full bg-sky-600 px-3 py-1 text-xs text-white transition-colors duration-300 ease-in-out hover:bg-blue-700">
-              Bucket
-            </Badge>
-            <div className="my-2 w-full border-b border-transparent"></div>
+          <div className="flex items-center">
+            <span className="mr-1 text-gray-400">Bucket:</span>
             <span className="text-white">{bucketName}</span>
           </div>
-          <div className="flex flex-col items-center space-y-2">
-            <Badge className="rounded-full bg-sky-600 px-3 py-1 text-xs text-white transition-colors duration-300 ease-in-out hover:bg-blue-700">
-              Trigger Factor
-            </Badge>
-            <div className="my-2 w-full border-b border-transparent"></div>
+          <div className="flex items-center">
+            <span className="mr-1 text-gray-400">Trigger Factor:</span>
             <span className="text-white">7.0</span>
           </div>
         </div>
-      </CardContent>
-      <CardFooter className="flex items-center justify-between p-4">
-        <Badge
-          className="bg-indigo-600 transition-colors duration-200 ease-in-out hover:bg-indigo-700"
-          variant="secondary"
-        >
-          {companyName}
-        </Badge>
-        <Button
-          onClick={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            toggleAccordion()
-          }}
-        >
-          {isAccordionOpen ? 'Hide details' : 'Quick details'}
-        </Button>
       </CardFooter>
-      <div ref={contentRef} className={`${isAccordionOpen ? openClasses : closedClasses}`}>
-        {isAccordionOpen && (
-          <div className="border-t border-gray-700 p-4">
-            <p className="text-gray-300">{description}</p>
-          </div>
-        )}
-      </div>
     </Card>
   )
 }

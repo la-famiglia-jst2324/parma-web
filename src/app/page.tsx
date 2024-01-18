3df67ee7a8ab5b2efd11727061831d76e1b80467
Hamza <chaoukihamza16@gmail.com>
2024-01-18 14:50:59 +0100

    Add commandInferface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@
 import React from 'react'
 import { Home } from '@/components/dashboard/Home'
 import { MainLayout } from '@/components/layout/MainLayout'
+import { CommandInterface } from '@/components/CommandInterface'
 
 export default function Page() {
   return (
     <MainLayout>
+      <CommandInterface />
       <Home />
     </MainLayout>
   )

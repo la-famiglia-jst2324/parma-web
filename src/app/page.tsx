ddfad3afeca753975c06f45c7e60de4a8c7094db
Hamza <chaoukihamza16@gmail.com>
2024-01-18 21:30:37 +0100

    fix:: remove double cmd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@
 import React from 'react'
 import { Home } from '@/components/dashboard/Home'
 import { MainLayout } from '@/components/layout/MainLayout'
-import { CommandInterface } from '@/components/CommandInterface'
 
 export default function Page() {
   return (
     <MainLayout>
-      <CommandInterface />
       <Home />
     </MainLayout>
   )

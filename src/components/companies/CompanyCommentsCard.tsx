6656a5f3d63809fdae9725052db5e750d6b1996b
Mustafa Asif <mustafaasif1@hotmail.com>
2024-01-24 01:45:31 +0100

    feat: Changed tremor component to shadcn (#206)
    Co-authored-by: Hamza <70699922+HamzaChx@users.noreply.github.com>

--- src/components/companies/CompanyCommentsCard.tsx
+++ src/components/companies/CompanyCommentsCard.tsx
@@ -1,11 +1,19 @@
 import React, { useContext, useState, useEffect } from 'react'
-import { SearchSelect, SearchSelectItem } from '@tremor/react'
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 import type { CompanyContextProps } from '../CompanyContext'
 import { CompanyContext } from '../CompanyContext'
-import CompanyCommentsCardItem from './CompanyCommentsCardItem'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '../ui/dropdown-menu'
+import { Button } from '../ui/button'
 import { getAnalyticsDataForCompany } from '@/services/measurement/measurementService'
-
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 interface CompanyMeasurement {
   id: number
   companyId: number
@@ -34,61 +42,82 @@ interface Comment {
 
 const CompanyCommentsCard: React.FC<CompanyCommentsCardProps> = ({ companyId, measurements }) => {
   const [comments, setComments] = useState<Comment[]>([])
+  const [selectedMeasurement, setSelectedMeasurement] = useState<string>(measurements[0]?.measurementName || '')
   const { companyData } = useContext(CompanyContext) as CompanyContextProps
   const companyName = companyData.name
-  const defaultMeasurement = String(measurements[0]?.id)
+  const defaultMeasurement = measurements[0]
 
-  const handleMeasurementChange = async (value: string) => {
+  const handleMeasurementChange = async (value: string, measurementName: string) => {
     try {
       const data = await getAnalyticsDataForCompany(value, companyId)
       setComments(data)
+      setSelectedMeasurement(measurementName)
     } catch (error) {
-      console.error('Failed to get the measurement data', error)
+      console.error('Failed to get the measurement data:', error)
     }
   }
 
   useEffect(() => {
-    handleMeasurementChange(defaultMeasurement)
-  }, [defaultMeasurement, companyId])
+    handleMeasurementChange(String(defaultMeasurement?.id), defaultMeasurement?.measurementName || '')
+  }, [defaultMeasurement?.id, companyId])
 
   return (
     <Card>
       <CardHeader>
-        <div className="flex items-center justify-between">
-          <CardTitle className="text-xl">Comments</CardTitle>
-          <SearchSelect
-            onValueChange={handleMeasurementChange}
-            placeholder={'Select comment measurement'}
-            className="w-96"
-            enableClear={false}
-            value={defaultMeasurement}
-          >
-            {measurements.map((measurement, index) => (
-              <SearchSelectItem key={index} value={String(measurement.id)}>
-                {measurement.measurementName}
-              </SearchSelectItem>
-            ))}
-          </SearchSelect>
+        <div className="flex items-start justify-between">
+          <div className="space-y-1">
+            <CardTitle className="text-xl">Graph</CardTitle>
+            <p className="text-sm">Selected Measurement: {selectedMeasurement}</p>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Select measurements</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Measurements</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                {measurements.map((measurement) => (
+                  <DropdownMenuItem
+                    key={measurement.id}
+                    onClick={() => handleMeasurementChange(String(measurement.id), measurement.measurementName)}
+                  >
+                    {measurement.measurementName}
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </CardHeader>
       <CardContent>
-        {comments.map((comment, index) => {
-          const commentData = comment[companyName]
-          let title = ''
-          let sentimentScore = 0
-          if (typeof commentData === 'object' && commentData !== null) {
-            title = (commentData as { value?: string }).value || ''
-            sentimentScore = (commentData as { sentimentScore?: number }).sentimentScore || 0
-          }
-          return (
-            <CompanyCommentsCardItem
-              key={index}
-              title={title}
-              timestamp={comment.date}
-              sentimentScore={sentimentScore}
-            />
-          )
-        })}
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Comment</TableHead>
+              <TableHead>Timestamp</TableHead>
+              <TableHead>Sentiment Score</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {comments.map((comment, index) => {
+              const commentData = comment[companyName]
+              let title = ''
+              let sentimentScore = 0
+              if (typeof commentData === 'object' && commentData !== null) {
+                title = (commentData as { value?: string }).value || ''
+                sentimentScore = (commentData as { sentimentScore?: number }).sentimentScore || 0
+              }
+              return (
+                <TableRow key={index}>
+                  <TableCell>{title}</TableCell>
+                  <TableCell>{comment.date}</TableCell>
+                  <TableCell>{sentimentScore}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
       </CardContent>
     </Card>
   )

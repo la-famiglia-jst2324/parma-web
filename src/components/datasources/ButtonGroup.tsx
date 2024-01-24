767f51cadadf01a26cfd765931c7514d4373327b
Hamza <70699922+HamzaChx@users.noreply.github.com>
2024-01-24 01:04:39 +0100

    feat: Add source scheduling on trigger (#201)
    Co-authored-by: GitHub Actions <action@github.com>

--- src/components/datasources/ButtonGroup.tsx
+++ src/components/datasources/ButtonGroup.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { Button } from '../ui/button'
 import ModalComponent from './Modals/DisableModal'
 import DeleteModal from './Modals/DeleteModal'
+import { triggerDataSource } from '@/services/datasource/datasourceService'
 
 interface DataProps {
   sourceName: string
@@ -12,6 +14,7 @@ interface DataProps {
 
 interface ButtonGroupProps {
   data: DataProps
+  refreshData: () => void
   handleSave: (updates: {
     newName: string
     newDescription: string
@@ -20,10 +23,20 @@ interface ButtonGroupProps {
   }) => Promise<void>
 }
 
-export const ButtonGroup: React.FC<ButtonGroupProps> = ({ data, handleSave }) => {
+export const ButtonGroup: React.FC<ButtonGroupProps> = ({ data, handleSave, refreshData }) => {
+  async function triggerDataSourceById(id: string) {
+    try {
+      const response = await triggerDataSource(id)
+      console.log(response)
+      refreshData()
+    } catch (error) {
+      console.error('Failed to trigger datasource:', error)
+    }
+  }
+
   return (
     <div className="mr-8 flex items-center justify-end space-x-4">
-      <Button className="text-green-500" variant="secondary">
+      <Button className="text-green-500" variant="secondary" onClick={() => triggerDataSourceById(data.id.toString())}>
         Schedule
       </Button>
       <ModalComponent

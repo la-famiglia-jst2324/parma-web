b5368bc19e5a85394f72ab29025e67103eda1828
Robin Holzinger <robin.holzinger@tum.de>
2024-01-28 23:40:25 +0100

    fix?

--- tests/api/database_connection.test.ts
+++ tests/api/database_connection.test.ts
@@ -1,16 +1,21 @@
+/**
+ * @jest-environment node
+ */
 import { Client } from 'pg'
 
 describe('Database connection', () => {
   it('should be reachable', async () => {
+    expect(true).toBe(true)
+    expect('postgresql://parma-prod-db:parma-prod-db@localhost:9000/parma-prod-db').toBeDefined()
     const client = new Client({
-      connectionString: process.env.POSTGRES_URL
+      connectionString: 'postgresql://parma-prod-db:parma-prod-db@localhost:9000/parma-prod-db'
     })
     await client.connect()
 
     try {
       const result = await client.query("SELECT 'Hello ' || $1 || '!' AS message", ['world'])
 
-      for await (const row of result.rows) {
+      for (const row of result.rows) {
         expect(row.message).toBe('Hello world!')
       }
     } finally {

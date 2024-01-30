b1d5301c23a8a16b1d2c5697bd081aea3c4c3090
Hamza <chaoukihamza16@gmail.com>
2024-01-30 16:41:10 +0100

    fix modal closing

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,6 +19,8 @@ const Sidebar: React.FC = () => {
   const { companies, setCompanies, buckets, setBuckets } = useContext(SideBarContext)
   const [clickBucket, setClickBucket] = useState(false)
   const [clickCompany, setClickCompany] = useState(false)
+  const [companyOpen, setCompanyOpen] = useState(false)
+  const [bucketOpen, setBucketOpen] = useState(false)
 
   const user = useContext(AuthContext)
   const uid = user !== 'loading' && user !== null ? user.uid : ''
@@ -101,6 +103,8 @@ const Sidebar: React.FC = () => {
                 </CollapsibleTrigger>
                 <CreateBucket
                   triggerButton={<Plus className="mr-2 h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-200" />}
+                  isOpen={bucketOpen}
+                  setOpen={setBucketOpen}
                 />
               </div>
               <CollapsibleContent>
@@ -149,6 +153,8 @@ const Sidebar: React.FC = () => {
                 </CollapsibleTrigger>
                 <CreateCompanyModal
                   triggerButton={<Plus className="mr-2 h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-200" />}
+                  isOpen={companyOpen}
+                  setOpen={setCompanyOpen}
                 />
               </div>
               <CollapsibleContent>
